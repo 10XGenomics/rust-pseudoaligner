e05336047ec0afa9c421b24d2c7dd40e060fd4a2
avi.srivastava <asrivastava@cs.stonybrook.edu>
2018-05-21 17:10:27 -0700

    first pass on the eqclass based dedup

--- src/main.rs
+++ src/main.rs
@@ -42,14 +42,13 @@ const MEM_SIZE: usize = 1;
 const REPORT_ALL_KMER: bool = false;
 pub type PrimDataType = u32;
 pub type KmerType = kmer::Kmer32;
-pub type DataType = SmallVec<[PrimDataType; 4]>;
+pub type DataType = PrimDataType;
 
 
 fn read_fasta(reader: fasta::Reader<File>)
               -> utils::Index<KmerType, Exts, DataType> {
 
-    let summarizer = debruijn::filter::CountFilterSmallInt::new(MIN_KMERS);
-    //let summarizer = filter::CountFilterSet::new(MIN_KMERS);
+    let summarizer = debruijn::filter::CountFilterEqClass::new(MIN_KMERS);
     let mut seqs = Vec::new();
     let mut trancript_counter: PrimDataType = 0;
 
@@ -99,49 +98,49 @@ fn read_fasta(reader: fasta::Reader<File>)
     utils::Index::new(dbg, phf)
 }
 
-fn process_reads(phf: &boomphf::BoomHashMap2<KmerType, Exts, DataType>,
-                 dbg: &DebruijnGraph<KmerType, DataType>,
-                 reader: fastq::Reader<File>){
-
-    let mut reads_counter = 0;
-    for result in reader.records() {
-        // obtain record or fail with error
-        let record = result.unwrap();
-        reads_counter += 1;
-
-        let seqs = DnaString::from_dna_string( str::from_utf8(record.seq()).unwrap() );
-
-        let mut eq_class: Vec<PrimDataType> = Vec::new();
-        for kmer in seqs.iter_kmers() {
-            //let (nid, _, _) = match dbg.find_link(kmer, Dir::Right){
-            //    Some(links) => links,
-            //    None => (std::usize::MAX, Dir::Right, false),
-            //};
-            //if nid != std::usize::MAX {
-            //    let labels = dbg.get_node(nid).data();
-            //    eq_class.extend(labels.clone().iter());
-            //    pdqsort::sort(&mut eq_class);
-            //    eq_class.dedup();
-            //}
-            let maybe_data = phf.get(&kmer);
-            match maybe_data {
-                Some((_, ref labels)) => {
-                    eq_class.extend(labels.clone().iter());
-                    pdqsort::sort(&mut eq_class);
-                    eq_class.dedup();
-                },
-                None => (),
-            }
-        }
-
-        if reads_counter % 100000 == 0 {
-            eprint!("\rDone Mapping {} reads", reads_counter);
-            io::stdout().flush().ok().expect("Could not flush stdout");
-        }
-        //println!("{:?} -> {:?}", record.id(), eq_class);
-    }
-    eprintln!();
-}
+//fn process_reads(phf: &boomphf::BoomHashMap2<KmerType, Exts, DataType>,
+//                 dbg: &DebruijnGraph<KmerType, DataType>,
+//                 reader: fastq::Reader<File>){
+//
+//    let mut reads_counter = 0;
+//    for result in reader.records() {
+//        // obtain record or fail with error
+//        let record = result.unwrap();
+//        reads_counter += 1;
+//
+//        let seqs = DnaString::from_dna_string( str::from_utf8(record.seq()).unwrap() );
+//
+//        let mut eq_class: Vec<PrimDataType> = Vec::new();
+//        for kmer in seqs.iter_kmers() {
+//            //let (nid, _, _) = match dbg.find_link(kmer, Dir::Right){
+//            //    Some(links) => links,
+//            //    None => (std::usize::MAX, Dir::Right, false),
+//            //};
+//            //if nid != std::usize::MAX {
+//            //    let labels = dbg.get_node(nid).data();
+//            //    eq_class.extend(labels.clone().iter());
+//            //    pdqsort::sort(&mut eq_class);
+//            //    eq_class.dedup();
+//            //}
+//            let maybe_data = phf.get(&kmer);
+//            match maybe_data {
+//                Some((_, ref labels)) => {
+//                    eq_class.extend(labels.clone().iter());
+//                    pdqsort::sort(&mut eq_class);
+//                    eq_class.dedup();
+//                },
+//                None => (),
+//            }
+//        }
+//
+//        if reads_counter % 100000 == 0 {
+//            eprint!("\rDone Mapping {} reads", reads_counter);
+//            io::stdout().flush().ok().expect("Could not flush stdout");
+//        }
+//        //println!("{:?} -> {:?}", record.id(), eq_class);
+//    }
+//    eprintln!();
+//}
 
 fn main() {
     let matches = App::new("De-bruijn-mapping")
@@ -152,8 +151,7 @@ fn main() {
              .short("f")
              .long("fasta")
              .value_name("FILE")
-             .help("Txome/Genome Input Fasta file")
-             .required(true))
+             .help("Txome/Genome Input Fasta file"))
         .arg(Arg::with_name("reads")
              .short("r")
              .long("reads")
@@ -183,16 +181,7 @@ fn main() {
     // obtain reader or fail with error (via the unwrap method)
     let index_file = matches.values_of("index").unwrap().next().unwrap();
 
-    if ! matches.is_present("make") {
-        // import the index if already present.
-        info!("Reading index from File: {:?}", index_file);
-        let input_dump: Result<utils::Index<KmerType, Exts, DataType>,
-                               Box<bincode::ErrorKind>> =
-            utils::read_obj(index_file);
-
-        ref_index = input_dump.expect("Can't read the index");
-    }
-    else{
+    if matches.is_present("make") {
         warn!("Creating the index, can take little time.");
         // if index not found then create a new one
         let reader = fasta::Reader::from_file(fasta_file).unwrap();
@@ -201,13 +190,24 @@ fn main() {
         info!("Dumping index into File: {:?}", index_file);
         utils::write_obj(&ref_index, index_file).expect("Can't dump the index");
     }
+    else{
+        // import the index if already present.
+        info!("Reading index from File: {:?}", index_file);
+        let input_dump: Result<utils::Index<KmerType, Exts, DataType>,
+                               Box<bincode::ErrorKind>> =
+            utils::read_obj(index_file);
 
-    // obtain reader or fail with error (via the unwrap method)
-    let reads_file = matches.value_of("reads").unwrap();
-    info!("Path for Reads FASTQ: {}\n\n", reads_file);
+        ref_index = input_dump.expect("Can't read the index");
 
-    let reads = fastq::Reader::from_file(reads_file).unwrap();
-    process_reads(ref_index.get_phf(), ref_index.get_dbg(), reads);
+        // obtain reader or fail with error (via the unwrap method)
+        let reads_file = matches.value_of("reads").unwrap();
+        info!("Path for Reads FASTQ: {}\n\n", reads_file);
+
+        //let reads = fastq::Reader::from_file(reads_file).unwrap();
+        process_reads(ref_index.get_phf(), ref_index.get_dbg(), reads);
+
+    }
+    info!("Finished Processing !")
 }
 
 #[cfg(test)]
@@ -221,7 +221,6 @@ mod tests{
     pub type PrimDataType = u32;
     pub type KmerType = kmer::Kmer32;
     pub type DataType = SmallVec<[PrimDataType; 4]>;
-
 
     #[test]
     fn test_kmer_search() {
