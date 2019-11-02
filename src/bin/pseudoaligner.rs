9205577cc03005edb9b6eaeb66846fc6663314ee
Patrick Marks <pmarks@gmail.com>
2019-11-02 16:24:20 -0700

    elide the hla/em stuff for merge back to master

--- src/bin/pseudoaligner.rs
+++ src/bin/pseudoaligner.rs
@@ -1,34 +1,19 @@
 // Copyright (c) 2018 10x Genomics, Inc. All rights reserved.
 
-extern crate bio;
-extern crate crossbeam_utils;
-extern crate debruijn;
-extern crate debruijn_mapping;
-extern crate docopt;
-extern crate failure;
-extern crate pretty_env_logger;
-extern crate rayon;
+use log::info;
+use serde::Deserialize;
 
-#[macro_use]
-extern crate log;
-
-#[macro_use]
-extern crate serde;
-
-// Import some modules
 use bio::io::{fasta, fastq};
 use docopt::Docopt;
 use failure::Error;
-use std::collections::HashMap;
 use std::{env, fs};
 use std::{path::PathBuf, str};
 
-use debruijn_mapping::{bam, em};
 use debruijn_mapping::{
     build_index::build_index, mappability::analyze_graph, pseudoaligner,
     pseudoaligner::process_reads,
 };
-use debruijn_mapping::{config, hla, utils};
+use debruijn_mapping::{config, utils};
 
 const PKG_NAME: &'static str = env!("CARGO_PKG_NAME");
 const PKG_VERSION: &'static str = env!("CARGO_PKG_VERSION");
@@ -37,14 +22,9 @@ De-bruijn-mapping
 
 Usage:
   pseudoaligner index -i <index> <ref-fasta>
-  pseudoaligner hla-index -i <index> <hla-fasta>
-  pseudoaligner comb-index -i <index> <ref-fasta> <hla-fasta>
   pseudoaligner map -i <index> <reads-fastq>
-  pseudoaligner map-bam [-o <outdir>]  -i <index> <bam> [<locus>]
-  pseudoaligner hla-map     [-o <outdir>] -i <index> <reads-fastq>
   pseudoaligner mappability [-o <outdir>] -i <index>
   pseudoaligner idxstats -i <index>
-  pseudoaligner em -i <index> -c <counts>
   pseudoaligner inspect -i <index> -c <counts> <genes>...
   pseudoaligner -h | --help | -v | --version
 
@@ -58,32 +38,23 @@ Options:
 #[derive(Clone, Debug, Deserialize)]
 struct Args {
     arg_ref_fasta: String,
-    arg_hla_fasta: String,
     arg_index: String,
     arg_counts: String,
-    arg_bam: String,
     arg_locus: Option<String>,
     arg_genes: Vec<String>,
     arg_reads_fastq: String,
     flag_outdir: Option<String>,
 
     cmd_index: bool,
-    cmd_hla_index: bool,
-    cmd_comb_index: bool,
 
-    cmd_em: bool,
     cmd_map: bool,
-    cmd_map_bam: bool,
     cmd_mappability: bool,
     cmd_idxstats: bool,
-    cmd_inspect: bool,
 
     // flag_long: bool,
     flag_version: bool,
     flag_v: bool,
 }
-
-const HLA_PATTERN: &'static str = "^HLA-.*";
 
 fn main() -> Result<(), Error> {
     let args: Args = Docopt::new(USAGE)
@@ -115,39 +86,7 @@ fn main() -> Result<(), Error> {
         info!("Writing index to disk");
         utils::write_obj(&index, args.arg_index)?;
         info!("Finished writing index!");
-    } else if args.cmd_hla_index {
-        info!("Building index from fasta");
-        let fasta = fasta::Reader::from_file(args.arg_hla_fasta)?;
-        let (seqs, tx_names, tx_allele_map) = hla::read_hla_cds(fasta)?;
-
-        let tx_gene_map = HashMap::new();
-
-        let index = build_index::<config::KmerType>(&seqs, &tx_names, &tx_gene_map)?;
-        info!("Finished building index!");
-
-        info!("Writing index to disk");
-        utils::write_obj(&(index, tx_allele_map), args.arg_index)?;
-        info!("Finished writing index!");
-    } else if args.cmd_comb_index {
-        info!("Building index from fasta");
-        let fasta = fasta::Reader::from_file(args.arg_ref_fasta)?;
-
-        // Load normal txs, with HLA genes filtered out.
-        let (mut seqs, mut tx_names, _tx_gene_map) =
-            utils::read_transcripts(fasta, Some(HLA_PATTERN))?;
-
-        info!("Building index from fasta");
-        let fasta = fasta::Reader::from_file(args.arg_hla_fasta)?;
-        let (hla_seqs, hla_tx_names, _hla_tx_allele_map) = hla::read_hla_cds(fasta)?;
-        let tx_gene_map = HashMap::new();
-
-        // WIP!!
-
-        // Tack on HLA to
-        seqs.extend(hla_seqs);
-        tx_names.extend(hla_tx_names);
-
-        let _index = build_index::<config::KmerType>(&seqs, &tx_names, &tx_gene_map)?;
+  
     } else if args.cmd_map {
         info!("Reading index from disk");
         let index = utils::read_obj(args.arg_index)?;
@@ -156,8 +95,7 @@ fn main() -> Result<(), Error> {
         info!("Mapping reads from fastq");
         let reads = fastq::Reader::from_file(args.arg_reads_fastq)?;
         process_reads::<config::KmerType, _>(reads, &index, outdir)?;
-    } else if args.cmd_map_bam {
-        map_bam(&args.clone())?;
+   
     } else if args.cmd_mappability {
         info!("Reading index from disk");
         let index = debruijn_mapping::utils::read_obj(args.arg_index)?;
@@ -178,111 +116,8 @@ fn main() -> Result<(), Error> {
             let eq = &index.eq_classes[*eqid as usize];
             println!("{}\t{}\t{}", e.node_id, e.sequence().len(), eq.len());
         }
-    } else if args.cmd_em {
-        let index: pseudoaligner::Pseudoaligner<config::KmerType> =
-            utils::read_obj(args.arg_index)?;
-        let mut eq_counts: bam::EqClassDb = utils::read_obj(&args.arg_counts)?;
-
-        let eqclass_counts = eq_counts.eq_class_counts(index.tx_names.len());
-
-        let weights = em::squarem(&eqclass_counts);
-
-        use std::fs::File;
-        use std::io::BufWriter;
-        use std::io::Write;
-        let mut weights_file = BufWriter::new(File::create("weights.tsv")?);
-
-        let mut weight_names: Vec<(f64, &String, u32)> = weights
-            .into_iter()
-            .enumerate()
-            .map(|(i, w)| (w, &index.tx_names[i], i as u32))
-            .collect();
-
-        weight_names.sort_by(|(wa, _, _), (wb, _, _)| (-wa).partial_cmp(&-wb).unwrap());
-
-        for (w, name, txid) in weight_names {
-            if w > 3e-3 {
-                // Find the tx's that overlap most strongly with this tx
-                let mut tx_counts: HashMap<u32, u32> = HashMap::new();
-
-                for (class, count) in &eqclass_counts.counts {
-                    if class.binary_search(&txid).is_ok() {
-                        for tx in class {
-                            let c = tx_counts.entry(*tx).or_insert(0);
-                            *c += count;
-                        }
-                    }
-                }
-
-                let my_counts = *(tx_counts
-                    .iter()
-                    .filter(|(ttx, _)| *ttx == &txid)
-                    .next()
-                    .unwrap()
-                    .1);
-
-                let mut neighbor_counts: Vec<_> = tx_counts
-                    .into_iter()
-                    .filter(|(ttx, _)| *ttx != txid)
-                    .collect();
-                neighbor_counts.sort_by_key(|(_, c)| -(*c as i32));
-
-                println!("X: {} {}", name, my_counts);
-                for i in 0..std::cmp::min(12, neighbor_counts.len()) {
-                    let (neighbor_id, count) = neighbor_counts[i].clone();
-                    if (count as f64) < 0.5 * my_counts as f64 {
-                        break;
-                    }
-                    println!("_: {}  {}", &index.tx_names[neighbor_id as usize], count);
-                }
-            }
-
-            writeln!(weights_file, "{}\t{}", name, w)?;
-        }
-    } else if args.cmd_inspect {
-        let index: pseudoaligner::Pseudoaligner<config::KmerType> =
-            utils::read_obj(args.arg_index)?;
-        let mut eq_counts: bam::EqClassDb = utils::read_obj(&args.arg_counts)?;
-
-        let eqclass_counts = eq_counts.eq_class_counts(index.tx_names.len());
-
-        let mut name_map = HashMap::new();
-        for (i, name) in index.tx_names.iter().enumerate() {
-            name_map.insert(name, i);
-        }
-
-        println!("genes: {:?}", args.arg_genes);
-        for s in args.arg_genes {
-            let id = name_map.get(&s).expect("couldn't find gene").clone() as u32;
-
-            println!("===== {} =====", s);
-            for (class, count) in &eqclass_counts.counts {
-                if count > &10 {
-                    if class.binary_search(&id).is_ok() {
-                        for tx in class {
-                            println!("g: {}", index.tx_names[*tx as usize]);
-                        }
-                        println!("count: {}", count);
-                    }
-                }
-            }
-        }
     }
 
     info!("Done!");
-    Ok(())
-}
-
-fn map_bam(args: &Args) -> Result<(), Error> {
-    info!("Reading index from disk");
-    let index = utils::read_obj(&args.arg_index)?;
-    info!("Finished reading index!");
-
-    info!("Mapping reads from BAM");
-
-    let path = PathBuf::from(args.flag_outdir.as_ref().unwrap());
-    //let p2: PathBuf = args.flag_outdir.as_ref().unwrap().into();
-    bam::map_bam(&args.arg_bam, index, &args.arg_locus, &path)?;
-
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 // Copyright (c) 2018 10x Genomics, Inc. All rights reserved.
 
-pub mod bam;
+
 pub mod build_index;
 pub mod config;
-pub mod em;
-pub mod hla;
-mod locus;
+
 pub mod mappability;
 pub mod pseudoaligner;
 pub mod utils;
