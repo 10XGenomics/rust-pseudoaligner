6ad6fcd4fdf45fa7f20e7712c910a3c560b6562c
Patrick Marks <pmarks@gmail.com>
2019-11-30 16:40:10 -0800

    make everything in Pseudoaligner public, pending a better API

--- src/pseudoaligner.rs
+++ src/pseudoaligner.rs
@@ -25,7 +25,7 @@ use crate::utils;
 pub struct Pseudoaligner<K: Kmer> {
     pub dbg: DebruijnGraph<K, EqClassIdType>,
     pub eq_classes: Vec<Vec<u32>>,
-    pub(crate) dbg_index: NoKeyBoomHashMap<K, (u32, u32)>,
+    pub dbg_index: NoKeyBoomHashMap<K, (u32, u32)>,
     pub tx_names: Vec<String>,
     pub tx_gene_mapping: HashMap<String, String>,
 }
